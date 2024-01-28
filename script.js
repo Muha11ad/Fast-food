44847df2461a5fa1cb6df100ba089eaa29e0fd14
Muha11ad <abdushukurovmukhammadjon99@gmail.com>
2024-01-28 15:47:07 +0500

    Done

--- script.js
+++ script.js
@@ -1,82 +1,141 @@
-const orderBtn = document.querySelector(".btn")
 const list = document.querySelector(".list") 
-orderBtn.addEventListener("click", ()=>{
-    let order =  (time , work) =>{
-        return new Promise((resolve , reject)=>{
-            if (true){
-                setTimeout(()=>{
-                    resolve(work())
-                },time)
-            } else{
-                reject(alert('click btn'))
-            }
-        })
-        }
-        order(2000, ()=> list.innerHTML = `<h3>Order accepted</h3>`)
-        .then(()=>{
-            return order(1500,()=>list.innerHTML = `<h3 style="text-align: center;">Products has been prepared<h3>`)
-        })
-        .then(()=>{
-            return order(1500,()=>  list.innerHTML = `<h3>Relax with procces<h3>`)
-        })
-        .then(()=>{
-            return order(2500,()=> list.innerHTML = `
+let openIsShop = false 
+     8 < new Date().getHours()  ? openIsShop = true : openIsShop = false 
+
+let order =  (time , work, Error) =>{
+    return new Promise((resolve , reject)=>{
+    openIsShop == true ? setTimeout(()=>{resolve(work())},time): reject(Error())})}
+
+async function make(){
+    try{
+    await  order(2000, () => list.innerHTML = `<h3>Order accepted</h3>`)
+
+    await  order(1500, () => list.innerHTML = `<h3 style="text-align: center;">Products has been prepared<h3>`) 
+           
+    await order(1500,()=>  list.innerHTML = `<h3>Relax with procces<h3>`)
+
+    await order(2500,()=> list.innerHTML = `
             <li class ="d-flex  justify-content-center flex-column align-items-center">
             <p style= "text-align:center; font-size:25px;" >We need bread burger</p>
             <img style="width:200px; height:184px;" src= "./images/1.PNG">
             </li>`)
-        })
-        .then(()=>{
-            return order(2500,()=>  list.innerHTML = `
-            <li class ="d-flex  justify-content-center flex-column align-items-center">
-            <p style= "text-align:center; font-size:25px;" >Prepend lettuce there</p>
-            <img style="width:200px; height:184px;" src= "./images/2.PNG">
-            </li>`)
-        })
-        .then(()=>{
-            return order(2500,()=> list.innerHTML = `
-            <li class ="d-flex  justify-content-center flex-column align-items-center">
-            <p style= "text-align:center; font-size:25px;" >put tomatoes 🍅</p>
-            <img style="width:200px; height:184px;" src= "./images/3.PNG">
-            </li>`)
-        })
-        .then(()=>{
-            return order(2500,()=> list.innerHTML = `
-            <li class ="d-flex  justify-content-center flex-column align-items-center">
-            <p style= "text-align:center; font-size:25px;" >Add meat to make it delicious </p>
-            <img style="width:200px; height:184px;" src= "./images/4.PNG">
-            </li>`
-        )})
-        .then(()=>{
-            return order(2500,()=>list.innerHTML = `
-            <li class ="d-flex  justify-content-center flex-column align-items-center">
-            <p style= "text-align:center; font-size:25px;" >netherlands cheese 🧀 </p>
-            <img style="width:200px; height:184px;" src= "./images/5.PNG">
-            </li>`
-        )})
-        .then(()=>{
-            return order(2500,()=> list.innerHTML = `
-            <li class ="d-flex  justify-content-center flex-column align-items-center">
-            <p style= "text-align:center; font-size:25px;" >Another lettuce to make it fresh  </p>
-            <img style="width:200px; height:184px;" src= "./images/6.PNG">
-            </li>`
-        )})
-        .then(()=>{
-            return order(2500,()=> alert("Wait please ⏳")
-        )})
-        .catch(()=>{
-           alert("Process has been stopped")
-          })
-        .finally(()=>{
-            return order(1500,()=>list.innerHTML = `
+
+    await order(2500,()=>  list.innerHTML = `
+             <li class ="d-flex  justify-content-center flex-column align-items-center">
+             <p style= "text-align:center; font-size:25px;" >Prepend lettuce there</p>
+             <img style="width:200px; height:184px;" src= "./images/2.PNG">
+             </li>`)
+
+    await order(2500,()=> list.innerHTML = `
+             <li class ="d-flex  justify-content-center flex-column align-items-center">
+             <p style= "text-align:center; font-size:25px;" >put tomatoes 🍅</p>
+             <img style="width:200px; height:184px;" src= "./images/3.PNG">
+             </li>`)
+
+    await order(2500,()=> list.innerHTML = `
+             <li class ="d-flex  justify-content-center flex-column align-items-center">
+             <p style= "text-align:center; font-size:25px;" >Add meat to make it delicious </p>
+             <img style="width:200px; height:184px;" src= "./images/4.PNG">
+             </li>`)
+
+    await order(2500,()=>list.innerHTML = `
+             <li class ="d-flex  justify-content-center flex-column align-items-center">
+             <p style= "text-align:center; font-size:25px;" >netherlands cheese 🧀 </p>
+             <img style="width:200px; height:184px;" src= "./images/5.PNG">
+             </li>`)
+
+    await order(2500,()=> list.innerHTML = `
+             <li class ="d-flex  justify-content-center flex-column align-items-center">
+             <p style= "text-align:center; font-size:25px;" >Another lettuce to make it fresh  <p>
+             <img style="width:200px; height:184px;" src= "./images/6.PNG">
+             </li>`)
+
+    await   order(2500,()=> alert("Wait please ⏳"))
+
+    await  order(1500,()=>list.innerHTML = `
             <li class ="d-flex  justify-content-center flex-column align-items-center">
             <p style= "text-align:center; font-size:25px;">buon appetito</p>
             <img style="width:200px; height:184px;" src= "./images/7.PNG">
-            </li>`
-        )})
+            </li>`)}
+    catch{
+    Error(alert("Cafe has been closed"))}}
 
 
-})
+// orderBtn.addEventListener("click", ()=>{
+//     let order =  (time , work) =>{
+//         return new Promise((resolve , reject)=>{
+//             if (true){
+//                 setTimeout(()=>{
+//                     resolve(work())
+//                 },time)
+//             } else{
+//                 reject(alert('click btn'))
+//             }
+//         })
+//         }
+//         order(2000, ()=> list.innerHTML = `<h3>Order accepted</h3>`)
+//         .then(()=>{
+//             return order(1500,()=>list.innerHTML = `<h3 style="text-align: center;">Products has been prepared<h3>`)
+//         })
+//         .then(()=>{
+//             return order(1500,()=>  list.innerHTML = `<h3>Relax with procces<h3>`)
+//         })
+//         .then(()=>{
+//             return order(2500,()=> list.innerHTML = `
+//             <li class ="d-flex  justify-content-center flex-column align-items-center">
+//             <p style= "text-align:center; font-size:25px;" >We need bread burger</p>
+//             <img style="width:200px; height:184px;" src= "./images/1.PNG">
+//             </li>`)
+//         })
+//         .then(()=>{
+//             return order(2500,()=>  list.innerHTML = `
+//             <li class ="d-flex  justify-content-center flex-column align-items-center">
+//             <p style= "text-align:center; font-size:25px;" >Prepend lettuce there</p>
+//             <img style="width:200px; height:184px;" src= "./images/2.PNG">
+//             </li>`)
+//         })
+//         .then(()=>{
+//             return order(2500,()=> list.innerHTML = `
+//             <li class ="d-flex  justify-content-center flex-column align-items-center">
+//             <p style= "text-align:center; font-size:25px;" >put tomatoes 🍅</p>
+//             <img style="width:200px; height:184px;" src= "./images/3.PNG">
+//             </li>`)
+//         })
+//         .then(()=>{
+//             return order(2500,()=> list.innerHTML = `
+//             <li class ="d-flex  justify-content-center flex-column align-items-center">
+//             <p style= "text-align:center; font-size:25px;" >Add meat to make it delicious </p>
+//             <img style="width:200px; height:184px;" src= "./images/4.PNG">
+//             </li>`
+//         )})
+//         .then(()=>{
+//             return order(2500,()=>list.innerHTML = `
+//             <li class ="d-flex  justify-content-center flex-column align-items-center">
+//             <p style= "text-align:center; font-size:25px;" >netherlands cheese 🧀 </p>
+//             <img style="width:200px; height:184px;" src= "./images/5.PNG">
+//             </li>`
+//         )})
+//         .then(()=>{
+//             return order(2500,()=> list.innerHTML = `
+//             <li class ="d-flex  justify-content-center flex-column align-items-center">
+//             <p style= "text-align:center; font-size:25px;" >Another lettuce to make it fresh  </p>
+//             <img style="width:200px; height:184px;" src= "./images/6.PNG">
+//             </li>`
+//         )})
+//         .then(()=>{
+//             return order(2500,()=> alert("Wait please ⏳")
+//         )})
+//         .catch(()=>{
+//            alert("Process has been stopped")
+//           })
+//         .finally(()=>{
+//             return order(1500,()=>list.innerHTML = `
+//             <li class ="d-flex  justify-content-center flex-column align-items-center">
+//             <p style= "text-align:center; font-size:25px;">buon appetito</p>
+//             <img style="width:200px; height:184px;" src= "./images/7.PNG">
+//             </li>`
+//         )})
+// })
 
 
 
